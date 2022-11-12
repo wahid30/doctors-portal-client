d65b31793588100bf36fc089d55b403ade7da78c
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-12 20:50:32 +0600

    makeappointment finished

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import chair from "../../../assets/images/chair.png";
+import PrimaryButton from "../../../components/PrimaryButton/PrimaryButton";
 
 const Banner = () => {
   return (
@@ -13,9 +14,7 @@ const Banner = () => {
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary">
-            Get Started
-          </button>
+          <PrimaryButton>Get Started</PrimaryButton>
         </div>
       </div>
     </div>
--- src/Pages/Home/MakeAppointment/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -1,23 +1,30 @@
 import React from "react";
+import doctor from "../../../assets/images/doctor.png";
+import appointment from "../../../assets/images/appointment.png";
+import PrimaryButton from "../../../components/PrimaryButton/PrimaryButton";
 
 const MakeAppointment = () => {
   return (
-    <section>
-      <div className="hero">
+    <section className="mt-32" style={{ background: `url(${appointment})` }}>
+      <div className="hero text-white">
         <div className="hero-content flex-col lg:flex-row">
           <img
-            src="https://placeimg.com/260/400/arch"
-            className="max-w-sm rounded-lg shadow-2xl"
+            src={doctor}
+            className="-mt-32 lg:w-1/2 hidden lg:block rounded-lg shadow-2xl"
             alt=""
           />
           <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <h4 className="text-lg text-primary font-bold">Appointment</h4>
+            <h1 className="text-4xl font-bold">Make an appointment Today</h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsumis that it has a more-or-less normal
+              distribution of letters,as opposed to using 'Content here, content
+              here', making it look like readable English. Many desktop
+              publishing packages and web page
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <PrimaryButton>Appointment</PrimaryButton>
           </div>
         </div>
       </div>
