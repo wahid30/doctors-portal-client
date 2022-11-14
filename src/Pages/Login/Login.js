46f05cff043548e9b04e2d5c327153170a7d2cc6
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-14 21:41:34 +0600

    apply form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  return <div>this is login</div>;
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+  return (
+    <div className="h-[800px] flex justify-center items-center">
+      <div>
+        <h2 className="text-4xl">Login</h2>
+        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              {...register("email")}
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              {...register("password")}
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+
+          <p>{data}</p>
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
