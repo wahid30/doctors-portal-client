b843e9e47eefc541f6bc57ebca6d2e5e08bfc37b
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-14 10:24:53 +0600

    conditional rendering added

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,12 +1,29 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AppointmentOption from "./AppointmentOption";
 
 const AvailableAppointments = ({ selectedDate }) => {
+  const [appointmentOptions, setAppointmentOptions] = useState([]);
+
+  useEffect(() => {
+    fetch("appointmentOptions.json")
+      .then((res) => res.json())
+      .then((data) => setAppointmentOptions(data));
+  });
+
   return (
     <section className="mt-16">
       <p className="text-center text-secondary font-bold">
         Available Appointments on {format(selectedDate, "PP")}
       </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+        {appointmentOptions.map((option) => (
+          <AppointmentOption
+            key={option._id}
+            appointmentOption={option}
+          ></AppointmentOption>
+        ))}
+      </div>
     </section>
   );
 };
