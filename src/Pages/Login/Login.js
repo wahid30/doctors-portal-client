b55151f1d8957fded77279a9cd7ae633a01b5b70
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-14 22:58:04 +0600

    added validation on login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
   const handleLogin = (data) => {
     console.log(data);
   };
@@ -18,9 +22,14 @@ const Login = () => {
             </label>
             <input
               type="email"
-              {...register("email")}
+              {...register("email", {
+                required: "Email Address is required",
+              })}
               className="input input-bordered w-full max-w-xs"
             />
+            {errors.email && (
+              <p className="text-red-600">{errors.email?.message}</p>
+            )}
           </div>
 
           <div className="form-control w-full max-w-xs">
@@ -29,12 +38,21 @@ const Login = () => {
             </label>
             <input
               type="password"
-              {...register("password")}
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters or longer",
+                },
+              })}
               className="input input-bordered w-full max-w-xs"
             />
             <label className="label">
               <span className="label-text">Forget Password?</span>
             </label>
+            {errors.password && (
+              <p className="text-red-600">{errors.password?.message}</p>
+            )}
           </div>
           <input
             className="btn btn-accent w-full"
