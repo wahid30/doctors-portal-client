64618f84e1e3f48d870004c179f15361628707ce
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-15 10:39:55 +0600

    signup authentication added

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,56 +1,87 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
+// import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const SignUp = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
-
+  const { createUser, updateUser } = useContext(AuthContext);
+  const [signUpError, setSignUPError] = useState("");
   const handleSignUp = (data) => {
     console.log(data);
+    setSignUPError("");
+    createUser(data.email, data.password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        // toast("User Created Successfully.");
+        const userInfo = {
+          displayName: data.name,
+        };
+        updateUser(userInfo)
+          .then(() => {})
+          .catch((err) => console.log(err));
+      })
+      .catch((error) => {
+        console.log(error);
+        setSignUPError(error.message);
+      });
   };
 
   return (
     <div className="h-[800px] flex justify-center items-center">
       <div className="w-96 p-7">
-        <h2 className="text-xl text-center">Signup</h2>
+        <h2 className="text-xl text-center">Sign Up</h2>
         <form onSubmit={handleSubmit(handleSignUp)}>
           <div className="form-control w-full max-w-xs">
             <label className="label">
+              {" "}
               <span className="label-text">Name</span>
             </label>
             <input
               type="text"
-              {...register("name")}
+              {...register("name", {
+                required: "Name is Required",
+              })}
               className="input input-bordered w-full max-w-xs"
             />
+            {errors.name && (
+              <p className="text-red-500">{errors.name.message}</p>
+            )}
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label">
+              {" "}
               <span className="label-text">Email</span>
             </label>
             <input
               type="email"
-              {...register("email", { required: true })}
+              {...register("email", {
+                required: true,
+              })}
               className="input input-bordered w-full max-w-xs"
             />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
           </div>
-
           <div className="form-control w-full max-w-xs">
             <label className="label">
+              {" "}
               <span className="label-text">Password</span>
             </label>
             <input
               type="password"
               {...register("password", {
-                required: "Password must required",
+                required: "Password is required",
                 minLength: {
                   value: 6,
-                  message: "Password must be at least 6 characters",
+                  message: "Password must be 6 characters long",
                 },
                 pattern: {
                   value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
@@ -66,18 +97,19 @@ const SignUp = () => {
           </div>
           <input
             className="btn btn-accent w-full mt-4"
-            value="signup"
+            value="Sign Up"
             type="submit"
           />
+          {signUpError && <p className="text-red-600">{signUpError}</p>}
         </form>
         <p>
-          Already Have an Account?
+          Already have an account{" "}
           <Link className="text-secondary" to="/login">
             Please Login
           </Link>
         </p>
         <div className="divider">OR</div>
-        <button className="btn btn-outline w-full">continue with google</button>
+        <button className="btn btn-outline w-full">CONTINUE WITH GOOGLE</button>
       </div>
     </div>
   );
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,12 +1,57 @@
-import React, { createContext } from "react";
-import { getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUser = (userInfo) => {
+    return updateProfile(user, userInfo);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("user observing");
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const authInfo = {
+    createUser,
+    signIn,
+    updateUser,
+    logOut,
+    user,
+    loading,
+  };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
