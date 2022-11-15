3f04ba0908e3c5a9eccbffaf2bec3cf996cc017a
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-15 09:57:18 +0600

    added signup

---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const handleSignUp = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="h-[800px] flex justify-center items-center">
+      <div className="w-96 p-7">
+        <h2 className="text-xl text-center">Signup</h2>
+        <form onSubmit={handleSubmit(handleSignUp)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              {...register("name")}
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              {...register("email", { required: true })}
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              {...register("password", {
+                required: "Password must required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters",
+                },
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+          </div>
+          <input
+            className="btn btn-accent w-full"
+            value="signup"
+            type="submit"
+          />
+        </form>
+        <p>
+          Already Have an Account?
+          <Link className="text-secondary" to="/login">
+            Please Login
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full">continue with google</button>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
